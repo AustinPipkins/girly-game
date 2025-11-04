d45abbb87f59ebc6712020b550f4f7acd1d417d3
AustinPipkins <auspip@outlook.com>
2025-11-04 17:04:16 -0600

    init

---
+++ src/client.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <cstring>
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <WS2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+    typedef int socklen_t;
+#else
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <netdb.h>
+    #define INVALID_SOCKET  (-1)
+    #define SOCKET_ERROR    (-1)
+    #define closesocket(s) close(s)
+    typedef int SOCKET;
+#endif
+
+void initSockets() {
+#ifdef _WIN32
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+    if (WSAStartup(ver, &wsData) != 0) {
+        std::cerr << "Can't start Winsock!" << std::endl;
+        exit(1);
+    }
+#endif
+}
+
+void cleanupSockets() {
+#ifdef _WIN32
+    WSACleanup();
+#endif
+}
+
+void receiveMessages(SOCKET sock) {
+    char buffer[4096];
+    while (true) {
+        memset(buffer, 0, 4096);
+        int bytesReceived = recv(sock, buffer, 4096, 0);
+        if (bytesReceived <= 0) {
+            std::cout << "Disconnected from server.\n";
+            break;
+        }
+        std::cout << "\n" << std::string(buffer, bytesReceived) << std::endl;
+        std::cout << "> " << std::flush;
+    }
+}
+
+int main() {
+    initSockets();
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Can't create socket!" << std::endl;
+        cleanupSockets();
+        return -1;
+    }
+
+    std::string serverIP;
+    std::cout << "Enter server IP: ";
+    std::cin >> serverIP;
+    std::cin.ignore();
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    inet_pton(AF_INET, serverIP.c_str(), &hint.sin_addr);
+
+    int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+    if (connResult == SOCKET_ERROR) {
+        std::cerr << "Can't connect to server!" << std::endl;
+        closesocket(sock);
+        cleanupSockets();
+        return -2;
+    }
+
+    std::thread receiver(receiveMessages, sock);
+    receiver.detach();
+
+    std::string userInput;
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, userInput);
+
+        if (userInput == "quit" || userInput == "exit") break;
+
+        send(sock, userInput.c_str(), userInput.size(), 0);
+    }
+
+    closesocket(sock);
+    cleanupSockets();
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <cstring>
+#include <algorithm>
+
+
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <WS2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+    typedef int socklen_t;
+#else
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+    #include <netdb.h>
+    #define INVALID_SOCKET  (-1)
+    #define SOCKET_ERROR    (-1)
+    #define closesocket(s) close(s)
+    typedef int SOCKET;
+#endif
+
+std::mutex clientsMutex;
+std::vector<SOCKET> clients;
+
+void initSockets() {
+#ifdef _WIN32
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+    if (WSAStartup(ver, &wsData) != 0) {
+        std::cerr << "Can't start Winsock!" << std::endl;
+        exit(1);
+    }
+#endif
+}
+
+void cleanupSockets() {
+#ifdef _WIN32
+    WSACleanup();
+#endif
+}
+
+// Broadcast message to all connected clients
+void broadcastMessage(const std::string &message, SOCKET sender) {
+    std::lock_guard<std::mutex> lock(clientsMutex);
+    for (SOCKET client : clients) {
+        if (client != sender) {
+            send(client, message.c_str(), message.size(), 0);
+        }
+    }
+}
+
+void handleClient(SOCKET clientSocket, sockaddr_in clientInfo) {
+    char clientIP[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientInfo.sin_addr, clientIP, INET_ADDRSTRLEN);
+    std::cout << "Client connected: " << clientIP 
+              << ":" << ntohs(clientInfo.sin_port) << std::endl;
+
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clients.push_back(clientSocket);
+    }
+
+    char buffer[4096];
+    while (true) {
+        memset(buffer, 0, 4096);
+        int bytesReceived = recv(clientSocket, buffer, 4096, 0);
+        if (bytesReceived <= 0) {
+            std::cout << "Client disconnected: " << clientIP << std::endl;
+            break;
+        }
+
+        std::string msg = "[" + std::string(clientIP) + "] " + std::string(buffer);
+        std::cout << msg << std::endl;
+        broadcastMessage(msg, clientSocket);
+    }
+
+    closesocket(clientSocket);
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clients.erase(std::remove(clients.begin(), clients.end(), clientSocket), clients.end());
+    }
+}
+
+int main() {
+    initSockets();
+
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == INVALID_SOCKET) {
+        std::cerr << "Can't create socket!" << std::endl;
+        cleanupSockets();
+        return -1;
+    }
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    hint.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR) {
+        std::cerr << "Can't bind to IP/port!" << std::endl;
+        closesocket(listening);
+        cleanupSockets();
+        return -2;
+    }
+
+    listen(listening, SOMAXCONN);
+    std::cout << "Server listening on port 54000..." << std::endl;
+
+    while (true) {
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+        SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        if (clientSocket == INVALID_SOCKET) continue;
+
+        std::thread t(handleClient, clientSocket, client);
+        t.detach();
+    }
+
+    closesocket(listening);
+    cleanupSockets();
+    return 0;
+}
